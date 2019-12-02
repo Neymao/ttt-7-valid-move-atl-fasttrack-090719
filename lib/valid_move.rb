de3c806955f4239bf07ae78c254a9576bc7d4aee
Ferney Mauricio Arenas <neypaisa@hotmail.com>
2019-12-02 12:16:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,10 +3,3 @@
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def some_new_fabulous_method
-    if position_taken?
-        execute something
-    else
-        execute something else
-    end
-end
